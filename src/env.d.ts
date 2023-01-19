ad4d896c5e1e67ab82e60b08b3e7f848130c37d4
Felix Ha <felixha@protonmail.com>
2023-01-19 16:21:58 +0700

    refactor: :recycle: cleanup

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,10 +3,6 @@ declare global {
     interface ProcessEnv {
       TELEGRAM_TOKEN: string;
       TWITTER_BEARER_TOKEN: string;
-      SERVER_URL: string;
-      PORT: string;
-      GITHUB_PERSONAL_ACCESS_TOKEN: string;
-      GITHUB_GIST_ID: string;
       MESSAGE_THREAD_ID: string;
       CHAT_ID: string;
     }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import 'dotenv-safe/config'
 import axios from 'axios'
 import { ETwitterStreamEvent, TwitterApi } from 'twitter-api-v2'
-// import express from 'express'
-// import bodyParser from 'body-parser'
 import {
   CHAT_ID,
-  // GITHUB_GIST_ID,
   MESSAGE_THREAD_ID,
-  // PORT,
   TELEGRAM_API,
   TWITTER_BEARER_TOKEN,
-  // URI,
 } from './consts'
-// import { getUsers, init, octokit } from './lib'
 
 const twitterClient = new TwitterApi(TWITTER_BEARER_TOKEN).readOnly.v2
 
@@ -36,6 +30,7 @@ const main = async (): Promise<void> => {
       { value: 'Drivechains' },
       { value: 'DRIVECHAIN' },
       { value: 'DRIVECHAINS' },
+      { value: 'bip300' },
       { value: 'BIP300' },
     ],
   })
@@ -56,100 +51,16 @@ const main = async (): Promise<void> => {
       return
     }
 
-    // Get Telegram users to message
-    // const users = await getUsers()
-
-    // if (!users) {
-    //   return
-    // }
-
-    // const tweetAuthorId = tweet.data.author_id
-    // const tweetAuthor = tweetAuthorId
-    //   ? await twitterClient.user(tweetAuthorId)
-    //   : undefined
-    // const tweetAuthorName = tweetAuthor?.data.username
-
-    // const tweetLink = `https://fxtwitter.com/${
-    //   tweetAuthorName || 'anyuser'
-    // }/status/${tweet.data.id}`
-    // const tweetContent = tweet.data.text
-
-    // for (const user of users) {
-    //   await axios
-    //     .post(`${TELEGRAM_API}/sendMessage`, {
-    //       chat_id: user.chatId,
-    //       // message_thread_id: MESSAGE_THREAD_ID,
-    //       // text: `${tweetContent} ${tweetLink}`,
-    //       text: tweetLink,
-    //     })
-    //     .then((resp) => console.log(resp))
-    //     .catch((err) => console.error(err))
-    // }
-
     // send msg to specific group
     await axios
       .post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: CHAT_ID,
         message_thread_id: MESSAGE_THREAD_ID,
-        // text: `${tweetContent} ${tweetLink}`,
         text: `https://fxtwitter.com/anyuser/status/${tweet.data.id}`,
       })
       .then((resp) => console.log(resp))
       .catch((err) => console.error(err))
   })
-
-  /* SERVER SETUP */
-  // const app = express()
-  // app.use(bodyParser.json())
-
-  // // Add new users who send /start
-  // app.post(URI, async (req, res) => {
-  //   const users = await getUsers()
-
-  //   if (!users) {
-  //     console.error('problem getting users')
-  //     return
-  //   }
-
-  //   if (!req.body.message?.text?.includes('/start')) {
-  //     console.log('spam, ignore')
-
-  //     return
-  //   }
-
-  //   const chatId = req.body.message?.chat.id
-
-  //   const existingChatIds = users.map((u) => u.chatId)
-
-  //   if (!existingChatIds.includes(chatId)) {
-  //     const newUsersArray = []
-
-  //     for (const existingChatId of existingChatIds) {
-  //       newUsersArray.push({ chatId: existingChatId })
-  //     }
-
-  //     newUsersArray.push({ chatId })
-
-  //     await octokit.request(`PATCH /gists/${GITHUB_GIST_ID}`, {
-  //       gist_id: GITHUB_GIST_ID,
-  //       description: 'Add users',
-  //       files: {
-  //         'users.json': {
-  //           content: JSON.stringify({ data: newUsersArray }),
-  //         },
-  //       },
-  //     })
-  //     return
-  //   }
-
-  //   return res.send()
-  // })
-
-  // app.listen(parseInt(PORT) || 3000, async () => {
-  //   console.log(`app running on port ${PORT || 3000}`)
-
-  //   await init()
-  // })
 }
 
 main().catch((error) => {
